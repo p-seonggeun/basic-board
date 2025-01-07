f7ff001c1eec25d4ceb3caa7c76a4f571f8abf3d
psg <1999byh@naver.com>
2025-01-07 10:25:55 +0900

    feat: BaseTimeEntity 추가

--- src/main/java/com/example/basicboard/BasicBoardApplication.java
+++ src/main/java/com/example/basicboard/BasicBoardApplication.java
@@ -2,8 +2,10 @@ package com.example.basicboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BasicBoardApplication {
 
     public static void main(String[] args) {
