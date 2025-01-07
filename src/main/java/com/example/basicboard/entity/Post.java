25d79354afd102a427e50ddc18ead62d696cfcca
psg <1999byh@naver.com>
2025-01-07 10:56:26 +0900

    feat: Post 엔티티 추가

---
+++ src/main/java/com/example/basicboard/entity/Post.java
@@ -0,0 +1,29 @@
+package com.example.basicboard.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static jakarta.persistence.GenerationType.*;
+import static lombok.AccessLevel.*;
+
+@Entity
+@NoArgsConstructor(access = PROTECTED)
+@Table(name = "posts")
+@Getter
+public class Post extends BaseTimeEntity {
+
+    @Id @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    private String title;
+    private String content;
+
+    public Post(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
