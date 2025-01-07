494df30b9eb06d2fa1bbf1aad3442cae2ccc34f5
psg <1999byh@naver.com>
2025-01-07 17:13:43 +0900

    feat: PostRepository 추가

---
+++ src/main/java/com/example/basicboard/repository/PostRepository.java
@@ -0,0 +1,17 @@
+package com.example.basicboard.repository;
+
+import com.example.basicboard.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+    Optional<Post> findPostById(Long id);
+
+    @Query("SELECT p FROM Post p JOIN FETCH p.comments WHERE p.id = :id")
+    Optional<Post> findPostWithCommentsById(@Param("id") Long id);
+
+}
