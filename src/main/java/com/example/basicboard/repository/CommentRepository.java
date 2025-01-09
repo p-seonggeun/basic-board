b552f43924ae880aae37aab2324b436687c18a7f
psg <1999byh@naver.com>
2025-01-09 11:29:05 +0900

    feat: CommentService 추가 및 CommentRepository 코드 추가

--- src/main/java/com/example/basicboard/repository/CommentRepository.java
+++ src/main/java/com/example/basicboard/repository/CommentRepository.java
@@ -2,7 +2,17 @@ package com.example.basicboard.repository;
 
 import com.example.basicboard.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    Optional<Comment> findCommentById(Long id);
+
+    @Query("SELECT c FROM Comment c JOIN FETCH c.post WHERE c.post.id = :id")
+    List<Comment> findCommentsByPostId(@Param("id") Long id);
 
 }
