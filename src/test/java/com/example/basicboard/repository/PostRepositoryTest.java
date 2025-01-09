13d8601b8e3b3a8f27c898944ceb5ac6b9453b07
psg <1999byh@naver.com>
2025-01-09 09:21:34 +0900

    feat: Post 수정 테스트 코드 추가

--- src/test/java/com/example/basicboard/repository/PostRepositoryTest.java
+++ src/test/java/com/example/basicboard/repository/PostRepositoryTest.java
@@ -27,10 +27,31 @@ class PostRepositoryTest {
         //then
         List<Post> result = repository.findAll();
         assertEquals(1, result.size());
-        Post findPostById = repository.findPostById(1L).orElse(null);
+        Post findPostById = repository.findById(1L).orElseThrow(() -> new IllegalArgumentException("Post not found"));
         Post findPost = result.getFirst();
         assertEquals("제목", findPost.getTitle());
         assertEquals(findPost, findPostById);
     }
+
+    @Test
+    public void 게시글_수정() throws Exception {
+        //given
+        Post post = new Post("제목", "내용");
+        repository.save(post);
+        Long postId = post.getId();
+
+        //when
+        Post findPost = repository.findById(postId).orElseThrow(() -> new IllegalArgumentException("Post not found"));
+        findPost.changePost("수정제목", "수정내용");
+
+        //then
+        repository.flush();
+        Post updatedPost = repository.findById(postId).orElseThrow(() -> new IllegalArgumentException("Post not found"));
+
+        assertEquals(updatedPost, findPost);
+        assertEquals("수정제목", updatedPost.getTitle());
+        assertEquals("수정내용", updatedPost.getContent());
+    }
+
 
 }
